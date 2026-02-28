0fef1f3ad753859830988685da5e2cfe89cced89
JesseCake <noodlepringle@gmail.com>
2026-02-28 23:05:49 +1100

    added launch script

---
+++ run_voice_node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script activates the venv and runs the voice node.
+# get the directory of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# activate the venv
+source "$SCRIPT_DIR/.venv/bin/activate"
+# run the voice node
+python "$SCRIPT_DIR/voice_node.py"
